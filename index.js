e3f0eeea46e59a0cbc54d898a672b5b0523e4d22
Alex30pro <alexandryopg87@gmail.com>
2024-09-11 11:02:43 -0300

    atualizado

--- index.js
+++ index.js
@@ -7,10 +7,14 @@ app.use(nomeCaixaAlta)
 const cepEndereco = require('./Middlewares/cepEndereco')
 const nomeCaixaAlta = require('./Middlewares/nomeCaixaAlta');
 
-app.post('/mercado', cepEndereco, (req, res) => {
-    res.json(req.body)
-})
-  
+const routerMercado = require('./Mercado/routerMercado');
+    app.use('/mercados', cepEndereco, routerMercado);
+
+const routerFornecedor = require('./Fornecedor/routerFornecedor');
+    app.use('/fornecedores', routerFornecedor);   
+    
+const routerProduto = require('./Produto/routerProduto');
+    app.use('/produtos', routerProduto);    
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta 3000`);
